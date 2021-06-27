468ebe199698cd5cc5af80311d983fd2fea9f3c7
DarshanGowda4696 <86545635+DarshanGowda4696@users.noreply.github.com>
2021-06-27 13:04:53 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header() {
+  return (
+      <header>
+        <h1>ShapeAI Bootcamp-Industrial Training and Internship</h1>
+      </header> 
+  );
+}
+
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Info() {
+  return (
+      <div className="note">
+        <h1>Javascript and React.Js</h1>
+        <p>A basic web dev React.Js Bootcamp by Saurya Sinha</p>
+        </div> 
+  );
+}
+
+export default Info;
